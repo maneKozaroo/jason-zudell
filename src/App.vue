156e0b0ce7407983c0d1070f1b652ac8c0cca443
maneKozaroo <jason.zudell@gmail.com>
2018-06-30 19:09:48 +0900

    Change to test App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,19 @@
   </div>
 </template>
 
+<script>
+  export default {
+    name: 'app',
+    methods: {
+      foo: (magicNumber) => {
+        if(magicNumber % 2 !== 0) {
+          return 'bar'
+        }
+        return 'foo'
+      }
+    }
+  }
+</script>
+
 <style lang="stylus">
 </style>
--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,16 +1,16 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import App from '@/App.vue'
 
-describe('HelloWorld.vue', () => {
+describe('App.vue', () => {
   it('returns "bar" for odd numbers', () => {
-    const wrapper = shallowMount(HelloWorld)
+    const wrapper = shallowMount(App)
     const expected = 'bar'
     const actual = wrapper.vm.foo(1)
     expect(expected).toMatch(actual)
   })
 
   it('returns "foo" for even numbers', () => {
-    const wrapper = shallowMount(HelloWorld)
+    const wrapper = shallowMount(App)
     const expected = 'foo'
     const actual = wrapper.vm.foo(2)
     expect(expected).toMatch(actual)
