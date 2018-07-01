8b1033c23d48bb38951305eccea8f6b1e056bfb1
maneKozaroo <jason.zudell@gmail.com>
2018-07-01 21:31:45 +0900

    Add data to resume

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <v-app id="app">
-    <v-toolbar app>
+
+    <v-toolbar class="hidden-sm-and-down" :app="$vuetify.breakpoint.mdAndUp">
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn flat>About</v-btn>
-        <v-btn flat>Skills</v-btn>
-        <v-btn flat>Experience</v-btn>
-        <v-btn flat>Contact</v-btn>
+        <v-btn flat @click="$vuetify.goTo('#about', { offset: -64 })">About</v-btn>
+        <v-btn flat @click="$vuetify.goTo('#skills', { offset: -64 })">Skills</v-btn>
+        <v-btn flat @click="$vuetify.goTo('#experience', { offset: -64 })">Experience</v-btn>
+        <v-btn flat @click="$vuetify.goTo('#contact', { offset: -64 })">Contact</v-btn>
       </v-toolbar-items>
       <v-spacer></v-spacer>
     </v-toolbar>
+
     <v-content>
       <resume-jumbotron></resume-jumbotron>
       <resume-about></resume-about>
@@ -17,6 +19,25 @@
       <resume-experience></resume-experience>
       <resume-contact></resume-contact>
     </v-content>
+
+    <v-bottom-nav app :value="$vuetify.breakpoint.smAndDown">
+      <v-btn flat color="teal" value="recent" @click="$vuetify.goTo('#about')">
+        <span>About</span>
+        <v-icon>info</v-icon>
+      </v-btn>
+      <v-btn flat color="teal" value="favorites" @click="$vuetify.goTo('#skills')">
+        <span>Skills</span>
+        <v-icon>list</v-icon>
+      </v-btn>
+      <v-btn flat color="teal" value="nearby" @click="$vuetify.goTo('#experience')">
+        <span>Experience</span>
+        <v-icon>work</v-icon>
+      </v-btn>
+      <v-btn flat color="teal" value="nearby" @click="$vuetify.goTo('#contact')">
+        <span>Contact</span>
+        <v-icon>place</v-icon>
+      </v-btn>
+    </v-bottom-nav>
   </v-app>
 </template>
 
--- src/containers/ResumeExperience.vue
+++ src/containers/ResumeExperience.vue
@@ -1,11 +1,101 @@
 <template>
-  <v-container class="blue" fluid>
-    <v-flex xs12>
+  <v-container fluid>
+    <v-flex id="experience" class="display-3 grey--text text--lighten-2" xs12>
       Experience
     </v-flex>
     <v-layout>
       <v-container>
         <v-layout row wrap>
+          <v-container>
+            <div class="timeline">
+              <div class="timeline-event">
+                <v-card class="timeline-content">
+                  <v-list two-line>
+                      <v-list-tile>
+                        <v-list-tile-content>
+                          <v-list-tile-title>Mobilus</v-list-tile-title>
+                          <v-list-tile-sub-title>Front-end (full-stack) web developer</v-list-tile-sub-title>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                          <v-list-tile-action-text>June 2017 - Present</v-list-tile-action-text>
+                        </v-list-tile-action>
+                      </v-list-tile>
+                  </v-list>
+                  <v-expansion-panel>
+                    <v-expansion-panel-content>
+                      <div slot="header"></div>
+                      <v-card>
+                        <ul class="ma-3">
+                            <li>Gathered and refined specifications and requirements based on technical needs.</li>
+                            <li>Created a React / React-Redux / Redux-Saga front-end app to display statistical information.</li>
+                            <li>Collaborated with a team of 3 to create a React / React-Redux / Redux-Saga front-end app to interface with AI services.</li>
+                            <li>Integrated with AI services such as IBM Watson Assistant, IBM Watson NLC, Google Dialogflow.</li>
+                        </ul>
+                      </v-card>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                </v-card>
+                <div class="timeline-badge blue white-text"><i class="material-icons">work</i></div>
+              </div>
+              <div class="timeline-event">
+                <v-card class="timeline-content">
+                  <v-list two-line>
+                    <v-list-tile>
+                      <v-list-tile-content>
+                        <v-list-tile-title>Fusion Systems Japan</v-list-tile-title>
+                        <v-list-tile-sub-title>Front-end (full-stack) web developer</v-list-tile-sub-title>
+                      </v-list-tile-content>
+                      <v-list-tile-action>
+                        <v-list-tile-action-text>March 2015 - May 2017</v-list-tile-action-text>
+                      </v-list-tile-action>
+                    </v-list-tile>
+                  </v-list>
+                  <v-expansion-panel>
+                    <v-expansion-panel-content>
+                      <div slot="header"></div>
+                      <v-card>
+                        <ul class="ma-3">
+                            <li>Provisioned and maintained an AWS OpsWorks stack for a Redmine instance.</li>
+                            <li>Created custom Redmine plugins for consultant time management, billing callint invoicing.</li>
+                            <li>Consulted with management on front-end frameworks and technologies.</li>
+                            <li>Led a team of 5 developers in the creation of a React / React-Redux / Redux-Saga front-end app for restaurant management.</li>
+                        </ul>
+                      </v-card>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                </v-card>
+                <div class="timeline-badge blue white-text"><i class="material-icons">work</i></div>
+              </div>
+              <div class="timeline-event">
+                <v-card class="timeline-content">
+                  <v-list two-line>
+                    <v-list-tile>
+                      <v-list-tile-content>
+                        <v-list-tile-title>Various clients</v-list-tile-title>
+                        <v-list-tile-sub-title>Freelance web developer</v-list-tile-sub-title>
+                      </v-list-tile-content>
+                      <v-list-tile-action>
+                        <v-list-tile-action-text>June 2008 - February 2015</v-list-tile-action-text>
+                      </v-list-tile-action>
+                    </v-list-tile>
+                  </v-list>
+                  <v-expansion-panel>
+                    <v-expansion-panel-content>
+                      <div slot="header"></div>
+                      <v-card>
+                        <ul class="ma-3">
+                            <li>Worked remotely with clients developing e-commerce, portfolio, and blog websites with multilingual functionalities.</li>
+                            <li>Provisioned AWS EC2 instances for php frameworks and Ruby on Rails installations.</li>
+                            <li>Installed, setup, and customized Drupal, Joomla, Magento, Wordpress, and Redmine instances.</li>
+                        </ul>
+                      </v-card>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                </v-card>
+                <div class="timeline-badge blue white-text"><i class="material-icons">work</i></div>
+              </div>
+            </div>
+          </v-container>
         </v-layout>
       </v-container>
     </v-layout>
@@ -17,3 +107,97 @@
     name: 'resume-experience'
   }
 </script>
+
+<style lang="stylus">
+  .timeline {
+    position: relative;
+  }
+
+  .timeline .timeline-event {
+    position: relative;
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+
+  .timeline .timeline-event .timeline-content {
+    position: relative;
+    width: calc(50% - 50px);
+  }
+
+  .timeline .timeline-event::before {
+    display: block;
+    content: "";
+    width: 2px;
+    height: calc(50% - 30px);
+    position: absolute;
+    background: #d2d2d2;
+    left: calc(50% - 1px);
+    top: 0;
+  }
+
+  .timeline .timeline-event::after {
+    display: block;
+    content: "";
+    width: 2px;
+    height: calc(50% - 30px);
+    position: absolute;
+    background: #d2d2d2;
+    left: calc(50% - 1px);
+    top: calc(50% + 30px);
+  }
+
+  .timeline .timeline-event:first-child::before {
+    display: none;
+  }
+
+  .timeline .timeline-event:last-child::after {
+    display: none;
+  }
+
+  .timeline .timeline-event:nth-child(even) .timeline-content {
+    margin-left: calc(50% + 50px);
+  }
+
+  .timeline .timeline-event:nth-child(odd) .timeline-content {
+    margin-left: 0;
+  }
+
+  .timeline .timeline-badge {
+    display: block;
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    background: #d2d2d2;
+    top: calc(50% - 20px);
+    right: calc(50% - 20px);
+    border-radius: 50%;
+    text-align: center;
+    cursor: default;
+  }
+
+  .timeline .timeline-badge i {
+    font-size: 25px;
+    line-height: 40px;
+  }
+
+  @media (max-width: 600px) {
+    .timeline .timeline-event .timeline-content {
+      width: calc(100% - 70px);
+    }
+    .timeline .timeline-event::before {
+      left: 19px;
+    }
+    .timeline .timeline-event::after {
+      left: 19px;
+    }
+    .timeline .timeline-event:nth-child(even) .timeline-content {
+      margin-left: 70px;
+    }
+    .timeline .timeline-event:nth-child(odd) .timeline-content {
+      margin-left: 70px;
+    }
+    .timeline .timeline-badge {
+      left: 0;
+    }
+  }
+</style>
